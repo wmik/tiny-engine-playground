2ed562a7a5e87e54ab682c12a9c65d6e7ad2b178
wmik <williemik.wmik@gmail.com>
2021-11-03 16:57:03 +0300

    ✨ feat: 2D engine base

---
+++ src/engine/engine.js
@@ -0,0 +1,19 @@
+function TinyEngine({ ctx }) {
+  this.ctx = ctx;
+  this.models = [];
+}
+
+TinyEngine.prototype.update = function (dt) {
+  this.models.forEach(function (model) {
+    model.update(dt);
+  }, this);
+};
+
+TinyEngine.prototype.render = function () {
+  this.ctx.save();
+  this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+  this.models.forEach(function (model) {
+    model.render(this.ctx);
+  }, this);
+  this.ctx.restore();
+};
