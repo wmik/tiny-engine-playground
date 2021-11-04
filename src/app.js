29da5479081dba1c90488b3c61ff3d27e594f1a5
wmik <williemik.wmik@gmail.com>
2021-11-04 08:26:43 +0300

    🧪feat: manual test dynamic boundary

--- src/app.js
+++ src/app.js
@@ -1,24 +1,24 @@
 let canvas = document.getElementById('world');
 
 let boundaries = [
-  new Boundary({
-    start: new Vector({ x: 500, y: 10 }),
-    end: new Vector({ x: 500, y: 300 }),
-    color: 'lime',
-    vx: 0
-  }),
-  new Boundary({
-    start: new Vector({ x: 100, y: 10 }),
-    end: new Vector({ x: 100, y: 300 }),
-    color: 'lime',
-    vx: 0
-  }),
-  new Boundary({
-    start: new Vector({ x: 100, y: 10 }),
-    end: new Vector({ x: 400, y: 200 }),
-    color: 'lime',
-    vx: 0
-  })
+  // new Boundary({
+  //   start: new Vector({ x: 500, y: 10 }),
+  //   end: new Vector({ x: 500, y: 300 }),
+  //   color: 'lime',
+  //   vx: 0
+  // }),
+  // new Boundary({
+  //   start: new Vector({ x: 100, y: 10 }),
+  //   end: new Vector({ x: 100, y: 300 }),
+  //   color: 'lime',
+  //   vx: 0
+  // }),
+  // new Boundary({
+  //   start: new Vector({ x: 100, y: 10 }),
+  //   end: new Vector({ x: 400, y: 200 }),
+  //   color: 'lime',
+  //   vx: 0
+  // })
 ];
 
 let to = new Car({
@@ -34,9 +34,37 @@ let to = new Car({
   vx: randomIntFromInterval(10, 20)
 });
 
+let toLeft = new Boundary({
+  start: new Vector({ x: to.pos.x, y: to.pos.y }),
+  end: new Vector({ x: to.pos.x, y: to.pos.y + to.h * 1.5 }),
+  color: 'yellow',
+  vx: to.vx
+});
+
+let toTop = new Boundary({
+  start: new Vector({ x: to.pos.x, y: to.pos.y }),
+  end: new Vector({ x: to.pos.x + to.w * 1.5, y: to.pos.y }),
+  color: 'pink',
+  vx: to.vx
+});
+
+let toRight = new Boundary({
+  start: new Vector({ x: to.pos.x + to.w * 1.5, y: to.pos.y }),
+  end: new Vector({ x: to.pos.x + to.w * 1.5, y: to.pos.y + to.h * 1.5 }),
+  color: 'red',
+  vx: to.vx
+});
+
+let toBottom = new Boundary({
+  start: new Vector({ x: to.pos.x, y: to.pos.y + to.h * 1.5 }),
+  end: new Vector({ x: to.pos.x + to.w * 1.5, y: to.pos.y + to.h * 1.5 }),
+  color: 'purple',
+  vx: to.vx
+});
+
 let fro = new Car({
   divisor: 5,
-  boundaries,
+  boundaries: boundaries.concat(toLeft, toTop, toRight, toBottom),
   pos: new Vector({
     x: 300,
     y: 130
@@ -48,7 +76,10 @@ let fro = new Car({
   vx: -randomIntFromInterval(5, 28)
 });
 
-let game = new Game({ canvas, models: [to, fro, ...boundaries] });
+let game = new Game({
+  canvas,
+  models: [to, fro, toLeft, toTop, toRight, toBottom, ...boundaries]
+});
 
 game.loop(0);
 
